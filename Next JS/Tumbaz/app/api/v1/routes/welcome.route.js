15b0df5a16b7998ffd09748cb91b4079247ee38d
programlia <programlia2901@gmail.com>
2025-08-11 16:07:18 +0700

    remove next  JS folder

--- Next JS/Tumbaz/app/api/v1/routes/welcome.route.js
+++
@@ -1,9 +0,0 @@
-// IMPORTS...
-const express = require("express");
-const welcome_controller = require("../controllers/welcome.controller");
-const router = express.Router();
-
-router.get("/" , welcome_controller);
-
-// EXPORTS...
-module.exports = router;
